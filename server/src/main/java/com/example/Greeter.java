a8e28744c578a160109e6eeae6cb19b11333614c
raviburra09 <38056041+raviburra09@users.noreply.github.com>
2018-09-25 11:31:48 -0400

    Update Greeter.java
    Fixed the method issue

--- server/src/main/java/com/example/Greeter.java
+++ server/src/main/java/com/example/Greeter.java
@@ -11,8 +11,12 @@ public class Greeter {
   public Greeter() {
 
   }
-
- 
+  
+/**
+ *@param someone name
+ *@retrun greeting string
+ */
+  
   public final String greet(final String someone) {
     return String.format("Hello, %s!", someone);
   }
